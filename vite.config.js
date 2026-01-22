81cb005d1a3902d60d6f22ceaa3997b8ef7bd851
Shuai Hao <Shuai.Hao@moodys.com>
2026-01-21 22:39:48 -0500

    Update vite config for runtime to work locally

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import dsv from '@rollup/plugin-dsv';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/me/',
   plugins: [react(), dsv()],
   build: {
     outDir: 'build',
   },
-});
+  ...mode === "development" && {
+    optimizeDeps: {
+      exclude: ["543fc9bac1bd4e19", "fb5ed6161a8a33b8"],
+    }
+  }
+}));
